e5405251adc8e9dde5079e76b4b8d835f2b97d46
Alxruc <ruchtialex@gmail.com>
2024-09-29 00:19:11 +0200

    style update and added a history

--- word.js
+++ word.js
@@ -22,6 +22,23 @@ function hasOneDayPassed(date) {
   return true;
 }
 
+function addToHistory(history, currentDate, currentWord, currentReading) {
+  history.unshift({ date: currentDate, word: currentWord, reading: currentReading });
+  localStorage.setItem('wordHistory', JSON.stringify(history));
+}
+
+function updateHistoryList(history) {
+  const historyList = document.getElementById("history-list");
+  historyList.innerHTML = '';
+  history.forEach(entry => {
+      const li = document.createElement('li');
+      li.textContent = `${entry.date}: ${entry.word} - ${entry.reading}`;
+      historyList.appendChild(li);
+  });
+}
+
+
+
 // This is called everytime someone clicks on the extension
 document.addEventListener('DOMContentLoaded', async function() {
   //for bug fixing
@@ -29,15 +46,18 @@ document.addEventListener('DOMContentLoaded', async function() {
   var date = new Date().toLocaleDateString();
 
   document.getElementById("todaysDate").textContent = `Today's date is: ${date}`;
+  let history = JSON.parse(localStorage.getItem('wordHistory')) || [];
   
   if(hasOneDayPassed(date)) {
     await getRandomWord();
+    addToHistory(history, date, localStorage.word, localStorage.reading)
   }
+
 
   document.getElementById("dailyWord").textContent = localStorage.word;
   document.getElementById("reading").textContent = localStorage.reading;
   
-  const button = document.getElementById("dictButton");
+  const button = document.getElementById("lookup-btn");
   button.addEventListener('click', () => {
     console.log("clicked");
     var link = "https://jisho.org/search/";
@@ -45,5 +65,28 @@ document.addEventListener('DOMContentLoaded', async function() {
     window.open(link);
   })
 
+  const historyBtn = document.getElementById("history-btn");
+
+  const mainCard = document.getElementById("main-view");
+  const historyCard = document.getElementById("history-view");
+  
+  // Show history
+  historyBtn.addEventListener('click', function() {
+    updateHistoryList(history);
+    mainCard.style.display = 'none';
+    historyCard.style.display = 'block';
+  });
+
+  const backBtn = document.getElementById("back-btn");
+
+  // Back to main card
+  backBtn.addEventListener('click', function() {
+      historyCard.style.display = 'none';
+      mainCard.style.display = 'block';
+  });
+
 });
+
+
+
 
